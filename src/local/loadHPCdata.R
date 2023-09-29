59e4b3c610c5a3368ba9590e4357c4cb6e7593d0
Jamie Hogg <jamie-hogg@hotmail.com>
2023-09-29 16:53:58 +1000

    Small additions

--- src/local/loadHPCdata.R
+++ src/local/loadHPCdata.R
@@ -6,16 +6,17 @@
 library(tidyverse)
 library(corrplot)
 rm(list = ls())
+gc()
 source("src/local/funs.R")
 
 # Set date
-cur_date <- "202309262"
+cur_date <- c("202309291")
 # 202309173 is 24 combos with no scale and no latent fixed
 # 202309153 is 24 combos with scale and latent fixed
 
 # list files
 files <- list.files(paste0("Z:/gscm/outputs/", cur_date, "/r"), full.names = T)
-files_fl <- files[!str_detect(files, "_f.rds|_fitonly.rds")]
+files_fl <- files[!str_detect(files, "_f.rds|_fitonly.rds|_tr.rds")]
 
 # read files
 out_all <- lapply(files_fl, readRDS)
@@ -31,6 +32,18 @@ perf <- bind_rows(perf_ll, .id = "ix")
 # read convergence data
 conv_ll <- lapply(1:length(out_all), FUN = function(x)out_all[[x]]$conv)
 conv <- bind_rows(conv_ll, .id = "ix")
+
+## Overall convergence ---- ##
+conv_ix <- (conv %>% 
+              filter(#max_Rhat < 1.1,
+                     n_Rhatgr1.05 == 0,
+                     set == "all"))$ix
+perf %>% 
+  filter(nu_div ==0,
+         nu_bfmi == 0) %>% 
+  mutate(conv_flag = ix %in% conv_ix) %>% 
+  view()
+## ---- ##
 
 # Convergence plot
 out_all[[9]]$summ %>% 
@@ -52,17 +65,17 @@ conv_ix <- (conv %>%
               filter(set == "all",
                      n_Rhatgr1.05 == 0))$ix
 perf %>% 
-  # filter(nu_div ==0,
-  #        nu_bfmi == 0,
-  #        ix %in% conv_ix, 
-  #        nu_tree ==0) %>% 
+  filter(nu_div ==0,
+         nu_bfmi == 0,
+         ix %in% conv_ix,
+         nu_tree ==0) %>%
   mutate(lower = elpd_loo - 1.96 * elpd_loo_se,
          upper = elpd_loo + 1.96 * elpd_loo_se) %>% 
   ggplot(aes(y = elpd_loo, ymin = lower, ymax = upper,
-             x = ix, col = as.factor(specific_latent_rho_fixed)))+
+             x = ix, col = as.factor(L)))+
   geom_errorbar()+
   geom_point()+
-  facet_grid(.~L)
+  facet_wrap(shared_latent_rho_fixed~specific_latent_rho_fixed)
 
 ggplot(perf, aes(y = lpd, x = ix, col = as.factor(specific_latent_rho_fixed)))+
   geom_point()+
--- t_src/wrangle/assessHPCresources.R
+++ t_src/wrangle/assessHPCresources.R
@@ -1,10 +1,15 @@
 
-dt <- "202309251"
-ff <- list.files(paste0("Z:/gscm/outputs/", dt, "/lyra_out"))
+dt <- "202309283"
+ff <- list.files(paste0("Z:/gscm/outputs/", dt, "/lyra_out"), full.names = T)
 ll <- list()
-for(i in 1:16){
-ll[[i]] <-suppressMessages(read_csv(paste0("Z:/gscm/outputs/", dt, "/lyra_out/GSCM_ix", i)) %>% 
+for(i in 1:length(ff)){
+ll[[i]] <-suppressMessages(read_csv(ff[i]) %>% 
   setNames(c("this")) %>% 
-  filter(str_detect(this, "Mem usage|Wall time")))
+  filter(str_detect(this, "Mem usage|Wall time"))%>% 
+  separate(this, into = c("Metric", "Value"), sep = " : ") %>% 
+  pivot_wider(names_from = Metric, values_from = Value) %>% 
+  setNames(c("wall_time", "mem_usage")))
 }
+
+bind_rows(ll) %>% view()
 
